699070e1cb661affd2808d9bbf6b31c0e977c286
LUIS <luisfgmtz@gmail.com>
2016-02-09 03:12:10 -0600

    POO_UIA

---
+++ practicas/P03/Ejercicio2.java
@@ -0,0 +1,15 @@
+public class Ejercicio2 {
+
+public static void main (String[] args){
+	double a =1 , b = 5,c = 2;
+	double numerador;
+	double denominador;
+	double resultado;
+	numerador = (b*b) - (4*a*c);//(2*a);
+	denominador = (2*a);
+	resultado = numerador / denominador;
+	System.out.println(resultado);
+
+
+}
+}
